8992a0c7c44bfba013f5b3fe9c7be9e6a9b78a06
Monkin <monkinsmurf@gmail.com>
2023-07-03 16:19:13 +0200

    Add: Header Curve SVG

--- src/Components/TopTab.jsx
+++ src/Components/TopTab.jsx
@@ -1,15 +1,30 @@
 import { Typography } from "./Tailwind";
 import { Cog8ToothIcon } from "@heroicons/react/24/solid";
+import WaveLight from "../assets/wave_light2.svg";
+import WaveDark from "../assets/wave_dark2.svg";
+import Logo from "../assets/logo.png";
 
 const TopTab = () => {
     return (
-        <div className="h-12 w-full px-4 bg-blue-400 dark:bg-gray-800 ">
-            <div className="flex flex-row h-full w-full justify-between items-center">
-                <Typography variant="h3">Indico Check-in</Typography>
+        <div className="h-28 w-full relative">
+            <div className="flex flex-row h-3/4 w-full justify-between items-center px-4">
+                <div className="flex flex-row h-full relative items-center">
+                    <img src={Logo} alt="Logo" className="h-2/3 mr-6"></img>
+
+                    <Typography variant="h3">Indico Check-in</Typography>
+                </div>
 
                 <Cog8ToothIcon
-                    className="h-1/2 dark:text-white active:opacity-50 hover:cursor-pointer"
+                    className="h-7 dark:text-white active:opacity-50 hover:cursor-pointer"
                     onClick={() => console.log("clicked")}
+                />
+            </div>
+
+            <div className="absolute top-0 left-0 w-full h-full z-[-1]">
+                <img
+                    src={WaveLight}
+                    alt="background"
+                    className="absolute w-full"
                 />
             </div>
         </div>
