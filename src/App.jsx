dc23eecb2cb5064a1a7e5d00e36b624b7147606e
Monkin <monkinsmurf@gmail.com>
2023-07-04 12:17:35 +0200

    Fixing format due to new linter

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,32 @@
-import React from "react";
-import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Homepage from "./pages/Homepage";
-import QrReader from "./pages/QrReader";
-import BottomTabs from "./Components/BottomTabs";
-import Background from "./Components/Background";
-import TopTab from "./Components/TopTab";
-import EventPage from "./pages/EventPage";
+import './App.css';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import Background from './Components/Background';
+import BottomTabs from './Components/BottomTabs';
+import TopTab from './Components/TopTab';
+import EventPage from './pages/EventPage';
+import Homepage from './pages/Homepage';
+import QrReader from './pages/QrReader';
 
 const App = () => {
-    return (
-        <div className="w-100 h-100">
-            <BrowserRouter basename="/">
-                <Background />
+  return (
+    <div className="w-100 h-100">
+      <BrowserRouter basename="/">
+        <Background />
 
-                <TopTab />
+        <TopTab />
 
-                <Routes>
-                    <Route path="/" element={<Homepage />} />
+        <Routes>
+          <Route path="/" element={<Homepage />} />
 
-                    <Route path="/qr-reader" element={<QrReader />} />
+          <Route path="/qr-reader" element={<QrReader />} />
 
-                    <Route path="/event/:id" element={<EventPage />} />
-                </Routes>
+          <Route path="/event/:id" element={<EventPage />} />
+        </Routes>
 
-                <BottomTabs />
-            </BrowserRouter>
-        </div>
-    );
+        <BottomTabs />
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
--- src/Components/TopTab.jsx
+++ src/Components/TopTab.jsx
@@ -1,34 +1,30 @@
-import { Typography } from "./Tailwind";
-import { Cog8ToothIcon } from "@heroicons/react/24/solid";
-import WaveLight from "../assets/wave_light2.svg";
-import WaveDark from "../assets/wave_dark2.svg";
-import Logo from "../assets/logo.png";
+import {Cog8ToothIcon} from '@heroicons/react/24/solid';
+import Logo from '../assets/logo.png';
+import WaveDark from '../assets/wave_dark2.svg';
+import WaveLight from '../assets/wave_light2.svg';
+import {Typography} from './Tailwind';
 
 const TopTab = () => {
-    return (
-        <div className="h-28 w-full relative">
-            <div className="flex flex-row h-3/4 w-full justify-between items-center px-4">
-                <div className="flex flex-row h-full relative items-center">
-                    <img src={Logo} alt="Logo" className="h-2/3 mr-6"></img>
+  return (
+    <div className="h-28 w-full relative">
+      <div className="flex flex-row h-3/4 w-full justify-between items-center px-4">
+        <div className="flex flex-row h-full relative items-center">
+          <img src={Logo} alt="Logo" className="h-2/3 mr-6"></img>
 
-                    <Typography variant="h3">Indico Check-in</Typography>
-                </div>
-
-                <Cog8ToothIcon
-                    className="h-7 dark:text-white active:opacity-50 hover:cursor-pointer"
-                    onClick={() => console.log("clicked")}
-                />
-            </div>
-
-            <div className="absolute top-0 left-0 w-full h-full z-[-1]">
-                <img
-                    src={WaveLight}
-                    alt="background"
-                    className="absolute w-full"
-                />
-            </div>
+          <Typography variant="h3">Indico Check-in</Typography>
         </div>
-    );
+
+        <Cog8ToothIcon
+          className="h-7 dark:text-white active:opacity-50 hover:cursor-pointer"
+          onClick={() => console.log('clicked')}
+        />
+      </div>
+
+      <div className="absolute top-0 left-0 w-full h-full z-[-1]">
+        <img src={WaveLight} alt="background" className="absolute w-full" />
+      </div>
+    </div>
+  );
 };
 
 export default TopTab;
