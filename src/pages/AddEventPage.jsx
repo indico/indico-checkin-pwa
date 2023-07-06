96e2f3234f3b499c5e8827a8dd50632377947cb3
Monkin <monkinsmurf@gmail.com>
2023-07-06 14:24:43 +0200

    Add: initial authorization code logic

--- src/pages/AddEventPage.jsx
+++ src/pages/AddEventPage.jsx
@@ -7,7 +7,7 @@ const AddEventPage = () => {
   const [data, setData] = useState('No Result');
   const [hasPermission, setHasPermission] = useState(true);
 
-  const onScanResult = (decodedText, _decodedResult) => {
+  const onScanResult = async (decodedText, _decodedResult) => {
     // handle scanned result
     // parse json object and detect if it is a valid event
     let eventData;
@@ -30,7 +30,37 @@ const AddEventPage = () => {
     const client = new OAuth2Client({
       server: base_url,
       clientId: client_id,
+      discoveryEndpoint: '.well-known/oauth-authorization-server',
+      fetch: window.fetch.bind(window), // Use the browser's native fetch API   TODO: Confirm this is correct
+
       // The tokenEndpoint and authorizationEndpoint are optional and will be inferred from the server's discovery document if not provided
+      authorizationEndpoint: 'https://indico.cern.ch/oauth/authorize',
+      tokenEndpoint: 'https://indico.cern.ch/oauth/token',
+    });
+
+    /**
+     * This generates a security code that must be passed to the various steps.
+     * This is used for 'PKCE' which is an advanced security feature.
+     *
+     * It doesn't break servers that don't support it, but it makes servers that
+     * so support it more secure.
+     *
+     * It's optional to pass this, but recommended.
+     */
+    const codeVerifier = await generateCodeVerifier();
+
+    // In a browser this might work as follows:
+    document.location = await client.authorizationCode.getAuthorizeUri({
+      // URL in the app that the user should get redirected to after authenticating
+      redirectUri: 'https://localhost:3000',
+
+      // Optional string that can be sent along to the auth server. This value will
+      // be sent along with the redirect back to the app verbatim.
+      state: 'some-string',
+
+      codeVerifier,
+
+      scope: [scope],
     });
 
     setData(decodedText);
