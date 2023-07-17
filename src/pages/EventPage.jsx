3cbf65522432ce31f4e7d0da92b1a9008dbaae19
Monkin <monkinsmurf@gmail.com>
2023-07-17 13:57:42 +0200

    Add: Working on auth requests

--- src/pages/EventPage.jsx
+++ src/pages/EventPage.jsx
@@ -1,8 +1,30 @@
+import {useEffect} from 'react';
 import {useLocation} from 'react-router-dom';
+import {authFetch} from '../utils/network';
 
 const EventPage = () => {
   const {state} = useLocation();
-  const {id, title, date, attendees} = state; // Destructure the state object containing the Event object
+  const {title, date, server_base_url} = state; // Destructure the state object containing the Event object
+
+  console.log('Server Base url:', server_base_url);
+
+  useEffect(() => {
+    // Fetch the event data from the server
+    const fetchEventData = async () => {
+      const response = await authFetch(
+        `${server_base_url}/api/checkin/event/83`,
+        null,
+        server_base_url
+      ); // TODO: Get the event ID
+      console.log('Response: ', response);
+    };
+
+    fetchEventData();
+
+    return () => {
+      // TODO: Abort the fetch request
+    };
+  }, []);
 
   return (
     <div className="mx-auto w-full h-full justify-center align-center mt-6">
--- src/utils/network.ts
+++ src/utils/network.ts
@@ -1,21 +1,33 @@
+import db from '../db/db';
+
 /**
  *
- * @param url
- * @param options
- * @returns
+ * @param url - The Request URL
+ * @param options - The options to pass to the fetch function
+ * @param serverBaseUrl - The base url of the server
+ * @returns The response from the fetch function
+ * @throws {Error} if an error occurs during the fetch
  */
-export const authFetch = async (url: string, options: RequestInit, token: string) => {
+export const authFetch = async (url: string, options: RequestInit, serverBaseUrl: string) => {
   try {
+    // Get the token from IndexedDB Server Table
+    const server = await db.servers.get({base_url: serverBaseUrl});
+    if (!server) {
+      throw new Error('Unable to retrieve the server');
+    }
+    const token = server.auth_token;
+
     // TODO: Fix this
     const response = await fetch(url, {
       ...options,
       headers: {
-        ...options.headers,
+        ...options?.headers,
         Authorization: `Bearer ${token}`,
       },
     });
     if (response.status === 401) {
       throw new Error('Unauthorized');
+      // TODO: Handle refreshing the token?
     }
 
     return response;
