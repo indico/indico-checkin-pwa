a1fedccd038bd8aea30074239b4fc67c07187d60
Monkin <monkinsmurf@gmail.com>
2023-07-18 18:05:58 +0200

    Finished Breadcrumbs

--- src/Components/Tailwind/Breadcrumbs.tsx
+++ src/Components/Tailwind/Breadcrumbs.tsx
@@ -1,4 +1,4 @@
-import {CalendarIcon} from '@heroicons/react/24/solid';
+import {CalendarIcon, ChevronRightIcon} from '@heroicons/react/24/solid';
 import Typography from './Typography';
 
 interface BreadcrumbsProps {
@@ -18,18 +18,20 @@ export const Breadcrumbs = ({routeNames = [], routeLinks, className = ''}: Bread
     <div className={parentClassName} aria-label="Breadcrumb">
       <CalendarIcon className="w-5 h-5 mr-1 text-primary" />
 
-      <ol
-        className="inline-flex items-center space-x-1 md:space-x-3 px-2 py-2 border border-gray-200 
-      rounded-lg bg-gray-50 dark:bg-gray-800 dark:border-gray-700"
-      >
+      <ol className="inline-flex items-center space-x-1 md:space-x-3">
         {routeNames.map((item, idx) => (
           <li className="inline-flex items-center" key={idx}>
-            <Typography
-              variant="body2"
-              className="items-center text-gray-800 hover:text-blue-600 dark:text-gray-400 dark:hover:text-white"
-            >
-              {item}
-            </Typography>
+            <>
+              {idx > 0 && <ChevronRightIcon className="w-5 h-5 mr-1 text-white" />}
+
+              <Typography
+                variant="body2"
+                className="items-center text-gray-800 hover:text-blue-600 dark:text-gray-400 dark:hover:text-white
+              px-2 py-2 border border-gray-200 rounded-lg bg-gray-50 dark:bg-gray-800 dark:border-gray-700"
+              >
+                {item}
+              </Typography>
+            </>
           </li>
         ))}
       </ol>
