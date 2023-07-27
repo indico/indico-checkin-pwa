441c22418df1b30497009d10f61a8b2e3996d0d6
Tomas Roun <tomas.roun@cern.ch>
2023-07-27 14:28:44 +0200

    Load settings from localStorage

--- src/context/SettingsProvider.jsx
+++ src/context/SettingsProvider.jsx
@@ -4,8 +4,16 @@ import PropTypes from 'prop-types';
 const SettingsContext = createContext({});
 
 export const SettingsProvider = ({children}) => {
-  const [darkMode, setDarkMode] = useState(false);
-  const [autoCheckin, setAutoCheckin] = useState(false);
+  // On render, check if the user has a theme preference. If not, check if their system is set to dark mode. If so, set the theme to dark.
+  // If neither, set the theme to light.
+  const storedTheme = localStorage.getItem('theme');
+  const isDarkMode =
+    storedTheme === 'dark' ||
+    (!storedTheme && window.matchMedia('(prefers-color-scheme: dark)').matches);
+  const [darkMode, setDarkMode] = useState(isDarkMode);
+  const [autoCheckin, setAutoCheckin] = useState(
+    JSON.parse(localStorage.getItem('autoCheckin') || false)
+  );
 
   return (
     <SettingsContext.Provider value={{darkMode, setDarkMode, autoCheckin, setAutoCheckin}}>
