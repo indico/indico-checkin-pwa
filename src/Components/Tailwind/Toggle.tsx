549086bc8d4033692f9b57feb2efa8403f55c1ae
Monkin <monkinsmurf@gmail.com>
2023-07-27 12:18:33 +0200

    Making Toggle more flexible

--- src/Components/Tailwind/Toggle.tsx
+++ src/Components/Tailwind/Toggle.tsx
@@ -2,9 +2,27 @@ interface ToggleProps {
   className?: HTMLDivElement['className'];
   checked: boolean;
   onClick: () => void;
+  rounded?: boolean;
 }
 
-export const Toggle = ({checked = false, className, onClick}: ToggleProps) => {
+export const Toggle = ({checked = false, className, rounded = true, onClick}: ToggleProps) => {
+  const roundedProps: HTMLDivElement['className'] = rounded
+    ? 'rounded-full after:rounded-full'
+    : '';
+
+  const scale = 2;
+
+  const dimensions = {
+    width: 11 * scale,
+    height: 6 * scale,
+    afterWidth: 5 * scale,
+    afterHeight: 5 * scale,
+    afterTop: 2 * scale,
+    afterLeft: 2 * scale,
+  };
+  const dimensionsClassNames: HTMLDivElement['className'] = `w-[${dimensions.width}px] h-[${dimensions.height}px] after:w-[${dimensions.afterWidth}px] \
+   after:h-[${dimensions.afterHeight}px] after:top-[${dimensions.afterTop}px] after:left-[${dimensions.afterLeft}px]`;
+
   return (
     <label className="relative inline-flex items-center cursor-pointer">
       <input
@@ -16,10 +34,10 @@ export const Toggle = ({checked = false, className, onClick}: ToggleProps) => {
         onChange={e => e.stopPropagation()}
       />
       <div
-        className="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 
-      rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] 
-      after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full 
-      after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"
+        className={`w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 \
+       peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] \
+      after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border \
+      after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600 ${roundedProps} ${className} ${dimensionsClassNames}}`}
       ></div>
     </label>
   );
--- src/pages/Events/ParticipantPage.tsx
+++ src/pages/Events/ParticipantPage.tsx
@@ -1,6 +1,7 @@
 import {useLocation, useNavigate} from 'react-router-dom';
 import {Typography} from '../../Components/Tailwind';
 import {Breadcrumbs} from '../../Components/Tailwind/Breadcrumbs';
+import {Toggle} from '../../Components/Tailwind/Toggle';
 import {ParticipantPageData} from '../../Models/EventData';
 
 const ParticipantPage = () => {
@@ -18,9 +19,8 @@ const ParticipantPage = () => {
 
   return (
     <div className="mx-auto w-full h-full justify-center align-center mt-3">
-      <div className="flex flex-row w-100 items-center justify-between">
+      <div className="flex flex-row w-100 items-center justify-between ml-2">
         <Breadcrumbs
-          className="ml-2"
           routeNames={[eventData.eventTitle, eventData.regFormLabel]}
           routeHandlers={[navigateBackTwice, navigateBack]}
         />
@@ -30,8 +30,18 @@ const ParticipantPage = () => {
         </Typography>
       </div>
 
-      <div>
+      <div className="mt-6 ml-2">
         <Typography variant="h2">{eventData.attendee.name}</Typography>
+
+        <Typography variant="h4" className="mt-6">
+          Checked In
+        </Typography>
+        <Toggle
+          checked={eventData.attendee.checked_in}
+          onClick={() => console.log('hello')}
+          rounded={false}
+          className="rounded"
+        />
       </div>
     </div>
   );
