8c007bc298fa147bca2bb63d93dae9bd1e7c97c9
Tomas Roun <tomas.roun@cern.ch>
2023-07-31 15:52:24 +0200

    Remove max width from regform cards

--- src/pages/Events/EventPage.jsx
+++ src/pages/Events/EventPage.jsx
@@ -162,7 +162,7 @@ const EventPage = () => {
     <div
       key={idx}
       onClick={() => onFormClick(idx)}
-      className="flex flex-wrap gap-2 justify-between max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow
+      className="flex flex-wrap gap-2 justify-between p-6 bg-white border border-gray-200 rounded-lg shadow
                  hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-600"
     >
       <div className="flex flex-1 items-center">
