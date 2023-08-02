537641724a119fb03c7d91471d9db6e7d05c26c8
Tomas Roun <tomas.roun@cern.ch>
2023-08-02 10:16:57 +0200

    Bottom navigation improvements

--- src/Components/BottomTabs.jsx
+++ src/Components/BottomTabs.jsx
@@ -1,55 +1,37 @@
-import {useState} from 'react';
 import {useNavigate} from 'react-router-dom';
 import {HomeIcon, QrCodeIcon} from '@heroicons/react/20/solid';
 
 const BottomTabs = () => {
-  const [activeTab, setActiveTab] = useState(0);
-
   const navigation = useNavigate();
 
   const onHomeClick = () => {
-    setActiveTab(0);
     navigation('/');
   };
 
   const onQrCodeClick = () => {
-    setActiveTab(1);
     navigation('/check-in');
   };
 
   return (
-    <div className="fixed bottom-0 left-0 z-50 w-full h-16 bg-gray-100 border-t border-gray-400 dark:bg-gray-800 dark:border-gray-600">
+    <div className="fixed bottom-0 left-0 z-50 w-full h-16 bg-white border-t border-gray-200 dark:bg-gray-700 dark:border-gray-600">
       <div className="grid h-full max-w-lg grid-cols-2 mx-auto font-medium">
         <button
           type="button"
           className="inline-flex flex-col items-center justify-center px-5 hover:bg-gray-50 dark:hover:bg-gray-800 group"
           onClick={onHomeClick}
         >
-          <HomeIcon
-            className={`h-6 w-6 text-gray-500 dark:text-gray-400 group-hover:text-blue-600 dark:group-hover:text-blue-500 
-            ${activeTab === 0 && 'text-blue-600 dark:text-blue-500'}`}
-          />
-          <span
-            className={`text-sm text-gray-500 dark:text-gray-400 group-hover:text-blue-600 dark:group-hover:text-blue-500
-            ${activeTab === 0 && 'text-blue-600 dark:text-blue-500'}`}
-          >
-            All Events
+          <HomeIcon className="h-6 w-6 mb-1 text-gray-500 dark:text-gray-400 group-hover:text-blue-600 dark:group-hover:text-blue-500" />
+          <span className="text-sm text-gray-500 dark:text-gray-400 group-hover:text-blue-600 dark:group-hover:text-blue-500">
+            All events
           </span>
         </button>
-
         <button
           type="button"
           className="inline-flex flex-col items-center justify-center px-5 hover:bg-gray-50 dark:hover:bg-gray-800 group"
           onClick={onQrCodeClick}
         >
-          <QrCodeIcon
-            className={`h-6 w-6 text-gray-500 dark:text-gray-400 group-hover:text-blue-600 dark:group-hover:text-blue-500
-          ${activeTab === 1 && 'text-blue-600 dark:text-blue-500'}`}
-          />
-          <span
-            className={`text-sm text-gray-500 dark:text-gray-400 group-hover:text-blue-600 dark:group-hover:text-blue-500
-            ${activeTab === 1 && 'text-blue-600 dark:text-blue-500'}`}
-          >
+          <QrCodeIcon className="h-6 w-6 mb-1 text-gray-500 dark:text-gray-400 group-hover:text-blue-600 dark:group-hover:text-blue-500" />
+          <span className="text-sm text-gray-500 dark:text-gray-400 group-hover:text-blue-600 dark:group-hover:text-blue-500">
             Check-in
           </span>
         </button>
