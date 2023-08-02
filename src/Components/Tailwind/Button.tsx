366aff84d831ecf5f30b69e5b1b42bb9f8c5f4ad
Monkin <monkinsmurf@gmail.com>
2023-08-02 16:06:57 +0200

    Add: Basic Modal w/ logic to show/hide

--- src/Components/Tailwind/Button.tsx
+++ src/Components/Tailwind/Button.tsx
@@ -7,7 +7,7 @@ interface ButtonProps {
 }
 
 const defaultBtnClassName: ComponentProps<'div'>['className'] =
-  'text-white bg-primary active:opacity-50 font-medium rounded-lg text-sm px-4 h-fit py-3 justify-self-center';
+  'text-white bg-primary active:opacity-50 font-medium rounded-lg text-sm px-4 h-fit py-3 justify-self-center ease-linear transition-all duration-150';
 
 const Button = ({children, className, onClick}: ButtonProps) => {
   const fullClassname = defaultBtnClassName + ' ' + (className ?? '');
--- src/Components/Tailwind/Modal/Modal.tsx
+++ src/Components/Tailwind/Modal/Modal.tsx
@@ -1,4 +1,6 @@
 import useAppState from '../../../hooks/useAppState';
+import Button from '../Button';
+import Typography from '../Typography';
 
 interface ModalProps {
   title: string;
@@ -6,42 +8,63 @@ interface ModalProps {
   btnText?: string;
 }
 
-const Modal = ({title, body, btnText = 'OK'}: ModalProps) => {
+const defaultTitle = 'An error occurred';
+const defaultBody = 'Something went wrong. Please try again later.';
+const defaultBtnText = 'OK';
+
+const Modal = ({
+  title = defaultTitle,
+  body = defaultBody,
+  btnText = defaultBtnText,
+}: ModalProps) => {
   const {showModal, setShowModal} = useAppState();
+
+  const stopPropagation = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    e.preventDefault();
+    e.stopPropagation();
+  };
+
+  const hideModal = () => {
+    setShowModal(false);
+  };
 
   return (
     showModal && (
-      <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
-        <div className="relative w-auto my-6 mx-auto max-w-3xl">
+      <div
+        className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none bg-black bg-opacity-40"
+        onClick={hideModal}
+      >
+        <div className="relative w-auto my-6 mx-5 max-w-3xl">
           {/*content*/}
-          <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+          <div
+            className="border-0 rounded-lg shadow-2xl relative flex flex-col w-full bg-white outline-none focus:outline-none"
+            onClick={stopPropagation}
+          >
             {/*header*/}
-            <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
-              <h3 className="text-3xl font-semibold">{title}</h3>
-              <button
-                className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
-                onClick={() => setShowModal(false)}
-              >
-                <span className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
-                  ×
-                </span>
-              </button>
+            <div className="flex items-start justify-between p-4 border-b border-solid border-slate-200 rounded-t">
+              <Typography variant="h3" className="font-semibold text-black dark:text-black">
+                {title}
+              </Typography>
             </div>
 
             {/*body*/}
-            <div className="relative p-6 flex-auto">
-              <p className="my-4 text-slate-500 text-lg leading-relaxed">{body}</p>
+            <div className="relative p-4 flex-auto">
+              <Typography
+                variant="body1"
+                className="my-4 text-slate-600 dark:text-slate-600 leading-normal"
+              >
+                {body}
+              </Typography>
             </div>
 
             {/*footer*/}
-            <div className="flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b">
-              <button
-                className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-                type="button"
-                onClick={() => setShowModal(false)}
+            <div className="flex items-center justify-end p-4 border-t border-solid border-slate-200 rounded-b">
+              <Button
+                className="font-bold uppercase shadow-lg outline-none min-w-[5rem]"
+                onClick={hideModal}
               >
                 {btnText}
-              </button>
+              </Button>
             </div>
           </div>
         </div>
