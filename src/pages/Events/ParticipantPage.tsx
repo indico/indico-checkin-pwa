a3fe559e7bfa08af577f389d3085e8be49e48325
Tomas Roun <tomas.roun@cern.ch>
2023-08-04 10:56:32 +0200

    Add basic structure for registration data

--- src/pages/Events/ParticipantPage.tsx
+++ src/pages/Events/ParticipantPage.tsx
@@ -1,6 +1,11 @@
 import {useCallback, useEffect, useMemo, useState} from 'react';
 import {useLocation, useNavigate, useParams} from 'react-router-dom';
-import {ShieldCheckIcon, CalendarDaysIcon} from '@heroicons/react/20/solid';
+import {
+  ShieldCheckIcon,
+  CalendarDaysIcon,
+  ChevronLeftIcon,
+  ChevronDownIcon,
+} from '@heroicons/react/20/solid';
 import {Typography} from '../../Components/Tailwind';
 import Badge from '../../Components/Tailwind/Badge';
 import {Breadcrumbs} from '../../Components/Tailwind/Breadcrumbs';
@@ -11,6 +16,137 @@ import {changeParticipantCheckIn, getEventDetailsFromIds} from '../../db/utils';
 import useAppState from '../../hooks/useAppState';
 import {ParticipantPageData} from '../../Models/EventData';
 import {authFetch} from '../../utils/network';
+
+const exampleSection = {
+  description: '',
+  fields: [
+    {
+      data: 'Tomas',
+      defaultValue: '',
+      description: '',
+      id: 880,
+      inputType: 'text',
+      title: 'First Name',
+    },
+    {
+      data: 'Roun',
+      defaultValue: '',
+      description: '',
+      id: 881,
+      inputType: 'text',
+      title: 'Last Name',
+    },
+    {
+      data: 'tomas.roun@cern.ch',
+      defaultValue: '',
+      description: '',
+      id: 882,
+      inputType: 'email',
+      title: 'Email Address',
+    },
+    {
+      data: 'CERN',
+      defaultValue: '',
+      description: '',
+      id: 883,
+      inputType: 'text',
+      title: 'Affiliation',
+    },
+    {
+      choices: [
+        {
+          _modified: true,
+          caption: 'Advanced Python',
+          extraSlotsPay: false,
+          id: 'a0a2fa62-9259-4b0b-97bc-070c4ef2db97',
+          isEnabled: true,
+          maxExtraSlots: 2,
+          placesLimit: 0,
+          price: 0,
+        },
+        {
+          caption: 'B',
+          extraSlotsPay: false,
+          id: '6d1f0af3-6f23-46d0-b652-3b72fac3ad3e',
+          isEnabled: true,
+          maxExtraSlots: 2,
+          placesLimit: 0,
+          price: 0,
+        },
+      ],
+      data: {'a0a2fa62-9259-4b0b-97bc-070c4ef2db97': 3},
+      defaultValue: {},
+      description: '',
+      id: 1065,
+      inputType: 'single_choice',
+      title: 'test extra',
+    },
+    {
+      data: 7,
+      defaultValue: null,
+      description: 'fdgfdgdfggd',
+      id: 1067,
+      inputType: 'number',
+      price: 0,
+      title: 'fgdgfd',
+    },
+    {
+      choices: [
+        {
+          caption: 'No accommodation',
+          id: '471ba223-e149-44a7-a076-4e1356dc0f79',
+          isEnabled: true,
+          isNoAccommodation: true,
+          placesLimit: 0,
+          price: 0,
+        },
+        {
+          caption: 'a',
+          id: 'abe74faf-5bc4-41c4-98bc-c02477f61795',
+          isEnabled: true,
+          isNoAccommodation: false,
+          placesLimit: 0,
+          price: 24.0,
+        },
+        {
+          caption: 'b',
+          id: 'bcc754d3-f45d-43f6-95d8-f0f0380628b3',
+          isEnabled: true,
+          isNoAccommodation: false,
+          placesLimit: 4,
+          price: 0,
+        },
+      ],
+      data: {
+        arrivalDate: '2022-12-20',
+        choice: 'bcc754d3-f45d-43f6-95d8-f0f0380628b3',
+        departureDate: '2022-12-24',
+        isNoAccommodation: false,
+      },
+      defaultValue: {
+        arrivalDate: null,
+        choice: '471ba223-e149-44a7-a076-4e1356dc0f79',
+        departureDate: null,
+        isNoAccommodation: true,
+      },
+      description: '',
+      id: 1068,
+      inputType: 'accommodation',
+      title: 'acc',
+    },
+    {
+      data: true,
+      defaultValue: false,
+      description: 'r354fdgf',
+      id: 1069,
+      inputType: 'checkbox',
+      price: 0,
+      title: 'cc',
+    },
+  ],
+  id: 879,
+  title: 'Personal Data',
+};
 
 const ParticipantPage = () => {
   const {state} = useLocation();
@@ -263,6 +399,10 @@ const ParticipantPage = () => {
               </div>
             </div>
           </div>
+          <div className="mt-6 mb-20">
+            {/* some weird TS error.. */}
+            <RegistrationSection section={exampleSection} />
+          </div>
         </>
       )}
     </div>
@@ -270,3 +410,134 @@ const ParticipantPage = () => {
 };
 
 export default ParticipantPage;
+
+type InputType =
+  | 'text'
+  | 'textarea'
+  | 'email'
+  | 'phone'
+  | 'number'
+  | 'bool'
+  | 'checkbox'
+  | 'date'
+  | 'country'
+  | 'file'
+  | 'single_choice'
+  | 'multi_choice'
+  | 'accommodation'
+  | 'accompanying_persons';
+
+interface Choice {
+  id: string; // uuid
+  caption: string;
+}
+
+interface Field {
+  id: number;
+  title: string;
+  description: string;
+  inputType: InputType;
+  data: any;
+  defaultValue: any;
+  price?: number;
+  choices?: Choice[]; // only for single/multi choice & accommodation
+}
+
+interface Section {
+  id: number;
+  title: string;
+  description: string;
+  fields: Field[];
+}
+
+function RegistrationSection({section}: {section: Section}) {
+  const {title, fields} = section;
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <div>
+      <h2 onClick={() => setIsOpen(o => !o)}>
+        <button
+          type="button"
+          className="flex items-center justify-between w-full p-5 font-medium text-left text-gray-500
+                     border border-b-0 border-gray-200 rounded-t-xl focus:ring-4 focus:ring-gray-200
+                     dark:focus:ring-gray-800 dark:border-gray-700 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800"
+          aria-expanded={isOpen}
+        >
+          <span>{title}</span>
+          {isOpen && <ChevronDownIcon className="w-6 h-6" />}
+          {!isOpen && <ChevronLeftIcon className="w-6 h-6" />}
+        </button>
+      </h2>
+      <div className={isOpen ? '' : 'hidden'}>
+        <div className="p-5 border border-b-0 border-gray-200 dark:border-gray-700 dark:bg-gray-900">
+          {fields.map(field => (
+            <RegistrationField key={field.id} field={field} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function RegistrationField({field}: {field: Field}) {
+  switch (field.inputType) {
+    case 'text':
+    case 'number':
+    case 'email':
+    case 'phone':
+      return <TextField {...field} />;
+    case 'checkbox':
+      return <CheckboxField {...field} />;
+    case 'single_choice':
+      return <SingleChoiceField {...field} />;
+    default:
+      console.log('unhandled field', field);
+      return null;
+  }
+}
+
+function TextField({title, data}: Field) {
+  return (
+    <div>
+      <Typography variant="h4">{title}</Typography>
+      <Typography variant="body1">{data}</Typography>
+    </div>
+  );
+}
+
+function CheckboxField({title, data}: Field) {
+  return (
+    <div>
+      <Typography variant="h4">{title}</Typography>
+      <Typography variant="body1">{data ? 'yes' : 'no'}</Typography>
+    </div>
+  );
+}
+
+function SingleChoiceField({title, choices, data}: Field) {
+  // data: {[uuid]: [number_of_choices]}
+  const selected = Object.keys(data)[0];
+
+  // nothing selected
+  if (selected === undefined) {
+    return (
+      <div>
+        <Typography variant="h4">{title}</Typography>
+      </div>
+    );
+  }
+
+  const amount = data[selected];
+  // find the caption of the selected choice
+  const caption = choices.find(choice => choice.id === selected).caption;
+
+  return (
+    <div>
+      <Typography variant="h4">{title}</Typography>
+      <Typography variant="body1">
+        {caption}: {amount}
+      </Typography>
+    </div>
+  );
+}
