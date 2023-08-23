bc01192501e52433b2ec322cd23bcd698ff9acb9
Tomas Roun <tomas.roun8@gmail.com>
2023-08-23 20:53:31 +0200

    Simplify error modal

--- src/Components/Tailwind/Modal/Modal.tsx
+++ src/Components/Tailwind/Modal/Modal.tsx
@@ -1,4 +1,5 @@
-import {ShieldExclamationIcon} from '@heroicons/react/24/outline';
+import {createPortal} from 'react-dom';
+import {ExclamationCircleIcon} from '@heroicons/react/24/outline';
 import useAppState from '../../../hooks/useAppState';
 import Button from '../Button';
 import Typography from '../Typography';
@@ -6,57 +7,35 @@ import Typography from '../Typography';
 const Modal = () => {
   const {showModal, modalData, disableModal} = useAppState();
 
-  const stopPropagation = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
-    e.preventDefault();
-    e.stopPropagation();
-  };
-
   return (
-    showModal && (
+    showModal &&
+    createPortal(
       <div
-        className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none bg-black bg-opacity-40"
+        className="fixed flex justify-center items-center inset-0 z-50 bg-black bg-opacity-70"
         onClick={disableModal}
       >
-        <div className="relative w-auto my-6 mx-5 max-w-3xl">
-          {/*content*/}
-          <div
-            className="border-0 rounded-lg shadow-2xl relative flex flex-col w-full bg-white outline-none focus:outline-none"
-            onClick={stopPropagation}
-          >
-            {/*header*/}
-            <div className="flex items-start justify-between px-4 pt-7 pb-5 border-b border-solid border-slate-200 rounded-t">
-              {/* Show Alert Icon */}
-              <div className="absolute -top-0 left-1/2 w-12 h-12 -translate-x-1/2 -translate-y-1/2 bg-dangerDark rounded-full shadow">
-                <ShieldExclamationIcon className="text-white p-1 text-center" />
-              </div>
-
-              <Typography variant="h3" className="font-semibold text-black dark:text-black">
-                {modalData.title}
-              </Typography>
-            </div>
-
-            {/*body*/}
-            <div className="relative p-4 flex-auto">
-              <Typography
-                variant="body1"
-                className="my-4 text-slate-600 dark:text-slate-600 leading-normal"
-              >
-                {modalData.body}
-              </Typography>
-            </div>
-
-            {/*footer*/}
-            <div className="flex items-center justify-end p-4 border-t border-solid border-slate-200 rounded-b">
-              <Button
-                className="font-bold uppercase shadow-lg outline-none min-w-[5rem]"
-                onClick={disableModal}
-              >
-                {modalData.btnText}
-              </Button>
-            </div>
+        <div
+          className="mx-4 rounded-lg shadow-2xl relative flex flex-col bg-white dark:bg-gray-700 rounded pt-10 pb-4 px-6"
+          onClick={e => e.stopPropagation()}
+        >
+          <div className="absolute top-0 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-dangerDark rounded-full">
+            <ExclamationCircleIcon className="p-1 w-12 min-w-[3rem] text-white" />
+          </div>
+          <Typography variant="h3">{modalData.title}</Typography>
+          <Typography variant="body1" className="mt-4">
+            {modalData.body}
+          </Typography>
+          <div className="flex justify-center pt-8">
+            <Button
+              className="font-bold shadow-lg outline-none min-w-[5rem] justify-center"
+              onClick={disableModal}
+            >
+              {modalData.btnText}
+            </Button>
           </div>
         </div>
-      </div>
+      </div>,
+      document.body
     )
   );
 };
