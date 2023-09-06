6fea2807c4774cb601f726d64b3eb1830d9db576
Tomas Roun <tomas.roun8@gmail.com>
2023-09-06 11:42:50 +0200

    Remove unused code

--- src/config.tsx
+++
@@ -1,6 +0,0 @@
-const config = Object.freeze({
-  BACKEND_URL: process.env.REACT_APP_BACKEND_URL || 'https://localhost:5000',
-  CLIENT_URL: process.env.REACT_APP_CLIENT_URL || 'https://localhost:3000',
-});
-
-export default config;
