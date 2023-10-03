a54732f718c6990fb61afa9fd59e4ae9f88a5760
Tomas Roun <tomas.roun@cern.ch>
2023-10-03 16:57:54 +0200

    Refactor Table component

--- src/Components/Tailwind/Table.tsx
+++ src/Components/Tailwind/Table.tsx
@@ -9,6 +9,7 @@ import {
 import {
   Filters,
   ParticipantFilters,
+  RegistrationState,
   ResultCount,
   ToggleFiltersButton,
   isDefaultFilterState,
@@ -20,11 +21,12 @@ import Typography from './Typography';
  * Props of a Single Row in the table
  */
 export interface rowProps {
+  id: number;
   fullName: string;
   checkedIn: boolean;
-  state: string;
+  state: RegistrationState;
   registrationDate: string;
-  onClick?: () => void;
+  onClick: () => void;
 }
 
 const Table = ({
@@ -40,9 +42,7 @@ const Table = ({
   filters: Filters;
   setFilters: (f: Filters) => void;
 }) => {
-  const [searchFocused, setSearchFocused] = useState(false);
   const [filtersVisible, setFiltersVisible] = useState(false);
-  const inputRef = useRef<HTMLInputElement>(null);
 
   const shownRows = useMemo(() => {
     if (rows.length === 0) {
@@ -84,91 +84,21 @@ const Table = ({
       });
   }, [searchValue, filters, rows]);
 
-  const onSearchChange = (e: ChangeEvent<HTMLInputElement>) => {
-    setSearchValue(e.target.value.toLowerCase());
-  };
-
-  const onKeyUp = (e: any) => {
-    if (e.key === 'Enter') {
-      e.target.blur();
-    }
-  };
-
-  const filteredRows = shownRows.map(({fullName, checkedIn, state, onClick}, idx) => {
-    const alternatingClass: HTMLElement['className'] =
-      idx % 2 === 1
-        ? 'bg-gray-100 dark:bg-gray-800 active:bg-gray-300 dark:active:bg-gray-600'
-        : 'bg-gray-200 dark:bg-gray-700 active:bg-gray-300 dark:active:bg-gray-600';
-
-    return (
-      <tr
-        key={idx}
-        style={{WebkitTapHighlightColor: 'transparent'}}
-        className={`cursor-pointer ${alternatingClass} transition-all
-                  active:bg-gray-300 dark:active:bg-gray-600`}
-        onClick={onClick}
-      >
-        <td className="p-4">
-          <div className="flex items-center justify-between">
-            <Typography
-              variant="body1"
-              className={state === 'rejected' || state === 'withdrawn' ? 'line-through' : ''}
-            >
-              {fullName}
-            </Typography>
-            {checkedIn && <CheckCircleIcon className="h-6 w-6 text-green-500" />}
-          </div>
-        </td>
-      </tr>
-    );
-  });
+  const filteredRows = shownRows.map(({id, fullName, checkedIn, state, onClick}, idx) => (
+    <Row
+      key={id}
+      fullName={fullName}
+      checkedIn={checkedIn}
+      state={state}
+      onClick={onClick}
+      isEven={idx % 2 === 0}
+    />
+  ));
 
   return (
     <div>
       <div className="flex gap-2 px-4 pb-2 pt-4">
-        <div className="relative grow">
-          {searchFocused && (
-            <div className="absolute inset-y-0 left-0 flex items-center pl-1">
-              <button type="button" className="p-1">
-                <ArrowSmallLeftIcon className="min-w-[2rem] text-gray-800 dark:text-gray-300" />
-              </button>
-            </div>
-          )}
-          {!searchFocused && (
-            <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
-              <MagnifyingGlassIcon className="h-5 w-5 text-gray-500" />
-            </div>
-          )}
-          {searchValue && (
-            <div className="absolute inset-y-0 right-0 flex items-center pr-1">
-              <button
-                type="button"
-                className="rounded-full p-2 transition-all active:bg-gray-200 dark:active:bg-gray-500"
-                onClick={() => {
-                  setSearchValue('');
-                  if (inputRef.current) {
-                    inputRef.current.focus();
-                  }
-                }}
-              >
-                <XMarkIcon className="min-w-[1.5rem] text-gray-800 dark:text-gray-300" />
-              </button>
-            </div>
-          )}
-          <input
-            type="text"
-            ref={inputRef}
-            className="text-md block w-full rounded-full border border-gray-300 bg-gray-50 py-2.5 pl-10 pr-2.5
-                       text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-transparent dark:bg-gray-700
-                       dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
-            placeholder="Search participants..."
-            value={searchValue}
-            onChange={onSearchChange}
-            onFocus={() => setSearchFocused(true)}
-            onBlur={() => setSearchFocused(false)}
-            onKeyUp={onKeyUp}
-          />
-        </div>
+        <SearchInput searchValue={searchValue} setSearchValue={setSearchValue} />
         <ToggleFiltersButton
           defaultState={isDefaultFilterState(filters)}
           filtersVisible={filtersVisible}
@@ -212,3 +142,114 @@ const Table = ({
 };
 
 export default Table;
+
+interface RowProps {
+  fullName: string;
+  checkedIn: boolean;
+  state: RegistrationState;
+  onClick: () => void;
+  isEven: boolean;
+}
+
+function Row({fullName, checkedIn, state, onClick, isEven}: RowProps) {
+  const alternatingClass: HTMLElement['className'] = isEven
+    ? 'bg-gray-100 dark:bg-gray-800 active:bg-gray-300 dark:active:bg-gray-600'
+    : 'bg-gray-200 dark:bg-gray-700 active:bg-gray-300 dark:active:bg-gray-600';
+
+  return (
+    <tr
+      style={{WebkitTapHighlightColor: 'transparent'}}
+      className={`cursor-pointer ${alternatingClass} transition-all active:bg-gray-300 dark:active:bg-gray-600`}
+      onClick={onClick}
+    >
+      <td className="p-4">
+        <div className="flex items-center justify-between">
+          <Typography
+            variant="body1"
+            className={state === 'rejected' || state === 'withdrawn' ? 'line-through' : ''}
+          >
+            {fullName}
+          </Typography>
+          {checkedIn && <CheckCircleIcon className="h-6 w-6 text-green-500" />}
+        </div>
+      </td>
+    </tr>
+  );
+}
+
+function SearchInput({
+  searchValue,
+  setSearchValue,
+}: {
+  searchValue: string;
+  setSearchValue: (v: string) => void;
+}) {
+  const [searchFocused, setSearchFocused] = useState(false);
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const onSearchChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setSearchValue(e.target.value.toLowerCase());
+  };
+
+  const onKeyUp = (e: any) => {
+    if (e.key === 'Enter') {
+      e.target.blur();
+    }
+  };
+
+  const clearSearch = () => {
+    setSearchValue('');
+    if (inputRef.current) {
+      inputRef.current.focus();
+    }
+  };
+
+  return (
+    <div className="relative grow">
+      {searchFocused && <LoseFocusButton />}
+      {!searchFocused && (
+        <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
+          <MagnifyingGlassIcon className="h-5 w-5 text-gray-500" />
+        </div>
+      )}
+      {searchValue && <ClearSearchButton onClick={clearSearch} />}
+      <input
+        type="text"
+        ref={inputRef}
+        className="text-md block w-full rounded-full border border-gray-300 bg-gray-50 py-2.5 pl-10 pr-2.5
+               text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-transparent dark:bg-gray-700
+               dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
+        placeholder="Search participants..."
+        value={searchValue}
+        onChange={onSearchChange}
+        onFocus={() => setSearchFocused(true)}
+        onBlur={() => setSearchFocused(false)}
+        onKeyUp={onKeyUp}
+      />
+    </div>
+  );
+}
+
+function LoseFocusButton() {
+  return (
+    <div className="absolute inset-y-0 left-0 flex items-center pl-1">
+      <button type="button" className="p-1">
+        <ArrowSmallLeftIcon className="min-w-[2rem] text-gray-800 dark:text-gray-300" />
+      </button>
+    </div>
+  );
+}
+
+function ClearSearchButton({onClick}: {onClick: () => void}) {
+  return (
+    <div className="absolute inset-y-0 right-0 flex items-center pr-1">
+      <button
+        type="button"
+        className="rounded-full p-2 transition-all active:bg-gray-200 dark:active:bg-gray-500"
+        onClick={onClick}
+      >
+        <XMarkIcon className="min-w-[1.5rem] text-gray-800 dark:text-gray-300" />
+      </button>
+    </div>
+  );
+}
