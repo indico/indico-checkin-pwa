4fea5c339c25811b0c3edd0764b8f8f33de9445f
Tomas Roun <tomas.roun@cern.ch>
2023-11-27 16:06:13 +0100

    Make registration state less confusing

--- src/pages/participant/RegistrationState.tsx
+++ src/pages/participant/RegistrationState.tsx
@@ -22,7 +22,7 @@ export function RegistrationState({state}: {state: _RegistrationState}) {
       className={`w-fit rounded-full bg-${color}-100 px-2.5 py-1 text-sm font-medium
                   text-${color}-800 dark:bg-${color}-900 dark:text-${color}-300`}
     >
-      {state}
+      Registration {state}
     </span>
   );
 }
