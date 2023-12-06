b2c50e6afda50234a2c2fec8fdc574fccf5a04f2
Tomas Roun <tomas.roun@cern.ch>
2023-12-06 13:25:40 +0100

    Use clearer registration status message

--- src/pages/participant/RegistrationState.tsx
+++ src/pages/participant/RegistrationState.tsx
@@ -2,12 +2,16 @@ import {RegistrationState as _RegistrationState} from '../../db/db';
 
 export function RegistrationState({state}: {state: _RegistrationState}) {
   let color;
+  let text: string = state;
 
   switch (state) {
     case 'complete':
       color = 'green';
       break;
     case 'pending':
+      color = 'yellow';
+      text = 'pending approval';
+      break;
     case 'unpaid':
       color = 'yellow';
       break;
@@ -22,7 +26,7 @@ export function RegistrationState({state}: {state: _RegistrationState}) {
       className={`w-fit rounded-full bg-${color}-100 px-2.5 py-1 text-sm font-medium
                   text-${color}-800 dark:bg-${color}-900 dark:text-${color}-300`}
     >
-      Registration {state}
+      Registration {text}
     </span>
   );
 }
