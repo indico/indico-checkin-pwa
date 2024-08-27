823787899f64497503b52144c5703e962a80299d
Ajob Kustra <ajob.edward.kustra@cern.ch>
2024-08-27 16:36:27 +0200

    dynamic env config

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,29 @@
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import viteTsconfigPaths from 'vite-tsconfig-paths';
 import tailwindcss from 'tailwindcss';
+import {defineConfig, loadEnv} from 'vite';
+import viteTsconfigPaths from 'vite-tsconfig-paths';
 
-export default defineConfig({
+export default defineConfig(({mode}) => {
+  // Currently our configuration only reads ENV variables which
+  // are prefixed with 'VITE_' (default, for security reasons).
+  const env = loadEnv(mode, process.cwd());
+  return {
     base: '',
     plugins: [react(), viteTsconfigPaths()],
-    server: {    
-        open: true,
-        port: 3000, 
+    server: {
+      open: true,
+      port: parseInt(env.VITE_PORT || '3000', 10),
     },
     build: {
-        cssCodeSplit: false
+      cssCodeSplit: false,
+    },
+    define: {
+      __APP_ENV__: JSON.stringify(env.APP_ENV),
     },
     css: {
-        postcss: {
-            plugins: [tailwindcss()]
-        }
-    }
-})
+      postcss: {
+        plugins: [tailwindcss()],
+      },
+    },
+  };
+});
