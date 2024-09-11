0b6a4b7bb6f57dc50acfbc3d02c5837706f4efbe
Ajob Kustra <ajob.edward.kustra@cern.ch>
2024-09-11 09:46:11 +0200

    build base path update vite

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig(({mode}) => {
   const env = loadEnv(mode, process.cwd());
 
   return {
-    base: '',
+    base: '/',
     plugins: [
       react(),
       viteTsconfigPaths(),
