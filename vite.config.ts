922a549f8e8fb521da8b9d00d00d61eedf7062f6
Ajob Kustra <ajob.edward.kustra@cern.ch>
2024-09-19 16:14:22 +0200

    extend lint command and fix issues

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
 import react from '@vitejs/plugin-react';
 import tailwindcss from 'tailwindcss';
 import {defineConfig, loadEnv} from 'vite';
+import {VitePWA} from 'vite-plugin-pwa';
 import viteTsconfigPaths from 'vite-tsconfig-paths';
-import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig(({mode}) => {
   // Currently our configuration only reads ENV variables which
@@ -21,8 +21,8 @@ export default defineConfig(({mode}) => {
         injectRegister: null,
         workbox: {
           globPatterns: ['**/*.{js,css,html,png,mp3,ttf}'],
-        }
-      })
+        },
+      }),
     ],
     test: {
       globals: true,
@@ -35,19 +35,19 @@ export default defineConfig(({mode}) => {
     },
     build: {
       cssCodeSplit: false,
-      minify: true
+      minify: true,
     },
     devOptions: {
-      enabled: false
-    },  
+      enabled: false,
+    },
     define: {
-      __APP_ENV__: JSON.stringify(env.APP_ENV)
+      __APP_ENV__: JSON.stringify(env.APP_ENV),
     },
     css: {
       postcss: {
         plugins: [tailwindcss()],
       },
     },
-    envDir: '.'
+    envDir: '.',
   };
 });
